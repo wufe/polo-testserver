3163ed5e60f3e00d2f8c564f207665c12200e36f
Simone Bembi <simone.bembi@gmail.com>
2021-03-01 19:28:39 +0100

    up

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.all('*', (req, res) => {
         <tr><td colspan=2 style="font-weight: bold; text-align: center">Repository</td></tr>
         <tr>
             <td>Branch</td>
-            <td>feature/my-watched-feature (9th version)</td>
+            <td>feature/my-watched-feature (10th version)</td>
         </tr>
         <tr><td colspan=2 style="font-weight: bold; text-align: center">General request info</td></tr>
         <tr>
