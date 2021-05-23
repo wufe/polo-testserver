34a1c7fb06cc8e7514e7d9ff20582554a55981e9
Simone Bembi <simone.bembi@gmail.com>
2021-05-23 11:46:06 +0200

    Intentionally making the build fail

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.all('*', (req, res) => {
         <tr><td colspan=2 style="font-weight: bold; text-align: center">Repository</td></tr>
         <tr>
             <td>Branch</td>
-            <td>Dev (rev-4)</td>
+            <td>Dev (rev-5)</td>
         </tr>
         <tr><td colspan=2 style="font-weight: bold; text-align: center">General request info</td></tr>
         <tr>
