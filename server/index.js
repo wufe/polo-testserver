5c058adf9a1e8712d493512cd97513a9c67edb06
Simone Bembi <simone.bembi@gmail.com>
2021-07-09 20:48:59 +0200

    up

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.all('*', (req, res) => {
         <tr><td colspan=2 style="font-weight: bold; text-align: center">Repository</td></tr>
         <tr>
             <td>Branch</td>
-            <td>Dev (rev-16)</td>
+            <td>Dev (rev-17)</td>
         </tr>
         <tr><td colspan=2 style="font-weight: bold; text-align: center">General request info</td></tr>
         <tr>
